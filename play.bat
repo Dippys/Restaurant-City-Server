9d45d1c391717ba2accc33284b68d713d923b11a
Dippys <ahmed@barrawi.xyz>
2026-08-27 16:10:41 +0400

    cleanup

--- play.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Launch the client so it loads FROM the local server. Because the game's
-REM resource URLs are relative, loading game.swf from the server makes every
-REM asset request resolve to the server too (and get logged in the dashboard).
-set "PLAYER=C:\flex\Player\flashplayer_32_sa_debug.exe"
-set "URL=http://localhost:8090/game.swf?cb=%RANDOM%%RANDOM%"
-echo Launching client -^> %URL%
-start "" "%PLAYER%" "%URL%"
--- start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Start the Restaurant City local RPC/asset server + dashboard.
-cd /d "%~dp0"
-echo Starting Restaurant City local server on http://localhost:8090 ...
-echo Dashboard: http://localhost:8090/__dash
-echo.
-npm start
